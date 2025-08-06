7069b79fb939c279115722134b84e861496ed0c0
Olayiwola Oyelowo <oyewolasolomon78@gmail.com>
2025-08-06 21:45:22 +0100

    got to 80% of project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,43 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    int image = Random().nextInt(5) + 1;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Ask Me Anything",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              image = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset("assets/images/ball$image.png"),
         ),
       ),
     );
